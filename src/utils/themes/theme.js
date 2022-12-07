cfa97858287b9888e7c1eae10d9e1b00698da403
Darek Mazur <dev@darekmazur.it>
2022-12-07 23:20:56 +0100

    create theme

---
+++ src/utils/themes/theme.js
@@ -0,0 +1,37 @@
+export const theme = {
+  colors: {
+    white: '#EFEFEF',
+    dark: '#444',
+    brown: '#93806F',
+    green: '#74917D',
+    lightGreen: '#79C791',
+  },
+
+  fonts: {
+    main: '"Montserrat", sans-serif',
+    header: '"Holtwood One SC", serif',
+    subheader: '"Cormorant Unicase", serif',
+  },
+
+  fontSize: {
+    xl: '4rem',
+    l: '3rem',
+    lm: '1.8rem',
+    m: '1.6rem',
+    s: '1.4rem',
+    xs: '1.2rem',
+  },
+
+  fontWeight: {
+    regular: '400',
+    bold: '600',
+    fat: '900',
+    thin: '200',
+  },
+
+  breakpoints: {
+    desktop: '1080px',
+    tablet: '800px',
+    mobile: '400px',
+  },
+};
