53e3e86156e0c7ac3350b48e52390df78d6ad37d
Darek Mazur <dev@darekmazur.it>
2022-12-09 00:03:08 +0100

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: 'src/assets/images/icon.png',
+        icon: 'src/assets/images/square_logo.png',
       },
     },
     'gatsby-transformer-remark',
