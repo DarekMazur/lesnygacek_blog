229a4a575361c2350639f2e0f054526a9fdf748f
Darek Mazur <dev@darekmazur.it>
2022-12-13 22:54:19 +0100

    fix Title hover

--- src/Components/Atoms/Title/Title.styles.js
+++ src/Components/Atoms/Title/Title.styles.js
@@ -4,8 +4,9 @@ export const StyledTitle = styled.div`
   color: ${({ theme, light }) => (light ? theme.colors.white : theme.colors.dark)};
   font-family: ${({ theme }) => theme.fonts.header};
   font-size: ${({ theme }) => theme.fontSize.xl};
-  padding: 2rem;
-  max-width: 40rem;
+  margin: 4rem;
+  padding: 1rem;
+  max-width: 45rem;
   text-align: center;
 
   h3 {
@@ -33,9 +34,12 @@ export const StyledTitle = styled.div`
       right: -2rem;
     }
 
-    &:hover::before,
     &:hover::after {
-      transform: scale(1.2);
+      transform: translate(3rem) scaleY(1.1);
+    }
+
+    &:hover::before {
+      transform: translate(-3rem) scaleY(1.1);
     }
   }
 `;
