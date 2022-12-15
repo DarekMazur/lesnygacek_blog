3d0cc6f66e715755ad38a3964c01234dcfce357c
Darek Mazur <dev@darekmazur.it>
2022-12-15 16:05:27 +0100

    refactor button style

--- src/Components/Atoms/Button/Button.styles.js
+++ src/Components/Atoms/Button/Button.styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledButton = styled.button`
   max-width: 40rem;
-  border: ${({ theme, light }) => (light ? `${theme.colors.white} solid .1rem` : `${theme.colors.dark} solid .1rem`)};
+  border: ${({ theme, light }) => `${light ? theme.colors.white : theme.colors.dark} solid .3rem`};
   background: transparent;
   color: ${({ theme, light }) => (light ? theme.colors.white : theme.colors.dark)};
   font-size: ${({ theme }) => theme.fontSize.l};
