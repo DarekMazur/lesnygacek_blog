cd17b54b2570ef480670ae21e517c64e8f846e93
Darek Mazur <dev@darekmazur.it>
2022-12-16 12:34:33 +0100

    create data structure

---
+++ src/data/mockData.js
@@ -0,0 +1,16 @@
+const mockData = [
+  {
+    id: '',
+    title: '',
+    createdAt: '',
+    publishedAt: '',
+    modifiedAt: '',
+    title: '',
+    category: '',
+    image: '',
+    slug: '',
+    description: '',
+    postBody: ``,
+    author: '',
+  },
+];
