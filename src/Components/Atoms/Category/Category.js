a0d3dfa86216f8b28e3499622e97e4ede592d9cb
Darek Mazur <dev@darekmazur.it>
2022-12-20 01:42:41 +0100

    create category component

---
+++ src/Components/Atoms/Category/Category.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Icon from '../Icon/Icon';
+
+const Category = () => {
+  return (
+    <div>
+      <Icon iconType="axe" size="default" />
+      <span>title</span>
+    </div>
+  );
+};
+
+export default Category;
