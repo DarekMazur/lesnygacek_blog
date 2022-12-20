4fb44671034c90282384285b9c6afeff74cc225c
Darek Mazur <dev@darekmazur.it>
2022-12-20 16:21:54 +0100

    test category

--- src/Components/Atoms/Category/Category.js
+++ src/Components/Atoms/Category/Category.js
@@ -1,24 +1,11 @@
 import React from 'react';
+import { categoryIcon, categoryTitle } from '../../../utils/helpers/category';
 import Icon from '../Icon/Icon';
 import { StyledCategory } from './Category.styles';
 
 const Category = ({ category, light }) => {
-  const categoryIcon = {
-    tools: 'axe',
-    trips: ['fas', 'hiking'],
-    thoughts: 'campfire',
-    others: 'backpack',
-  };
-
-  const categoryTitle = {
-    tools: 'Narzędzia',
-    trips: 'Wyprawy',
-    thoughts: 'Przemyślenia',
-    others: 'Inne',
-  };
-
   return (
-    <StyledCategory light={light}>
+    <StyledCategory light={light} data-testid={category}>
       <Icon icon={Array.isArray(categoryIcon[category]) ? 'fa' : null} iconType={categoryIcon[category]} size="small" light={light} />
       <span>{categoryTitle[category]}</span>
     </StyledCategory>
