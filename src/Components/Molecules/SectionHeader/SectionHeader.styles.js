7c32537bcf664a38601bf930901b5c86e0d96cc6
Darek Mazur <dev@darekmazur.it>
2022-12-20 01:22:22 +0100

    update padding style

--- src/Components/Molecules/SectionHeader/SectionHeader.styles.js
+++ src/Components/Molecules/SectionHeader/SectionHeader.styles.js
@@ -4,6 +4,7 @@ export const StyledSectionHeader = styled.section`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: 0 3rem;
 
   &:nth-of-type(even) {
     flex-direction: row-reverse;
