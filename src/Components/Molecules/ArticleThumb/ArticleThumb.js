410688519b768aea9e9d4ea3e94fbc212b4fa3fc
Darek Mazur <dev@darekmazur.it>
2022-12-21 19:31:59 +0100

    add styles to article thumb

--- src/Components/Molecules/ArticleThumb/ArticleThumb.js
+++ src/Components/Molecules/ArticleThumb/ArticleThumb.js
@@ -1,12 +1,11 @@
 import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
-import Category from '../../Atoms/Category/Category';
-import Title from '../../Atoms/Title/Title';
 import ArticleDetails from '../ArticleDetails/ArticleDetails';
+import { StyledArticleThumb } from './ArticleThumb.style';
 
 const ArticleThumb = ({ articleData }) => {
   return (
-    <div style={{ backgroundColor: '#EFEFEF', padding: '3rem', width: 'calc((100% - 10rem) / 2)', margin: '3rem' }}>
+    <StyledArticleThumb>
       <StaticImage
         src="https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
         alt=""
@@ -16,15 +15,9 @@ const ArticleThumb = ({ articleData }) => {
         aspectRatio={4 / 3}
         formats={['auto', 'webp']}
       />
-      {/* ---- molecule ---- */}
-      {/* <div style={{ display: 'flex', flexDirection: 'column', JustifyContent: 'center', alignItems: 'center' }}>
-        <p>{articleData.publishedAt}</p>
-        <Title sub>Lorem Ipsum</Title>
-        <Category category={articleData.category} />
-      </div> */}
       <ArticleDetails date={articleData.publishedAt} title={articleData.title} category={articleData.category} />
       <div>{articleData.description}</div>
-    </div>
+    </StyledArticleThumb>
   );
 };
 
--- src/Components/Molecules/ArticleThumb/ArticleThumb.style.js
+++ src/Components/Molecules/ArticleThumb/ArticleThumb.style.js
@@ -1,1 +1,8 @@
 import styled from 'styled-components';
+
+export const StyledArticleThumb = styled.div`
+  background-color: ${({ theme }) => theme.colors.white};
+  padding: 3rem;
+  width: calc((100% - 10rem) / 2);
+  margin: 3rem;
+`;
