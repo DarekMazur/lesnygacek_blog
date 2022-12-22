48c98db0e613676ef5b76f19fa4d8cc68876dd1e
Darek Mazur <dev@darekmazur.it>
2022-12-22 17:21:32 +0100

    create header sketch

--- src/Components/Organisms/Header/Header.js
+++ src/Components/Organisms/Header/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Logo from '../../Atoms/Logo/Logo';
+import MenuListItem from '../../Atoms/MenuListItem/MenuListItem';
+import MenuList from '../../Molecules/MenuList/MenuList';
 
 const Header = () => {
   return (
     <header>
-      <h1>HEADER</h1>
+      <Logo logo={'long'} />
+      <MenuList>
+        <MenuListItem>Item</MenuListItem>
+        <MenuListItem>Item</MenuListItem>
+        <MenuListItem>Item</MenuListItem>
+      </MenuList>
+      <div>
+        search
+        <MenuList>
+          <MenuListItem social="youtube" />
+          <MenuListItem social="instagram" />
+          <MenuListItem social="facebook" />
+        </MenuList>
+      </div>
     </header>
   );
 };
--- src/Components/Templates/Layout/Layout.js
+++ src/Components/Templates/Layout/Layout.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Logo from '../../Atoms/Logo/Logo';
 import Copyright from '../../Molecules/Copyright/Copyright';
+import Header from '../../Organisms/Header/Header';
 
 const Layout = ({ children }) => {
   return (
     <>
-      <header style={{ backgroundColor: '#444' }}>
+      {/* <header style={{ backgroundColor: '#444' }}>
         <Logo logo={'long'} light />
         <Logo logo={'long'} light={'dark'} />
         <Logo logo={'long'} />
         <Logo logo={'long'} size="8" />
         <Logo logo={'long'} size="5" />
         <Logo logo={'square'} />
-      </header>
+      </header> */}
+      <Header />
       {children}
       <footer style={{ backgroundColor: '#93806F' }}>
         <Copyright pageCopy />
