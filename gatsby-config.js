abcc15062d0cdd06138b114c455c48e94a50b3e9
Darek Mazur <dev@darekmazur.it>
2022-12-29 01:58:06 +0100

    create production version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,60 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Leśny Gacek`,
-    siteUrl: `https://www.lesnygacek.pl`,
+    title: `lesnygacek_landing`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     'gatsby-plugin-styled-components',
     {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'G-QLELH6H8Z8',
-      },
-    },
-    'gatsby-plugin-image',
-    {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: 'src/images/icon.png',
-      },
-    },
-    'gatsby-transformer-remark',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: './src/images/',
-      },
-      __key: 'images',
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'pages',
-        path: './src/pages/',
-      },
-      __key: 'pages',
-    },
-    {
-      resolve: `gatsby-plugin-google-fonts-v2`,
-      options: {
-        fonts: [
-          {
-            family: 'Montserrat',
-            weights: ['200', '400', '600', '900'],
-            variable: ['ital'],
-          },
-          {
-            family: 'Holtwood One SC',
-            weights: ['400'],
-          },
-          {
-            family: 'Cormorant Unicase',
-            weights: ['400'],
-          },
-        ],
+        icon: 'src/images/favicon.png',
       },
     },
   ],
