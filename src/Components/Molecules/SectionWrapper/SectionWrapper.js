0ee240aa599905e65ef9767f7066cc38bc1ba1f9
Darek Mazur <dev@darekmazur.it>
2023-01-05 22:24:40 +0100

    fix section wrapper align

--- src/Components/Molecules/SectionWrapper/SectionWrapper.js
+++ src/Components/Molecules/SectionWrapper/SectionWrapper.js
@@ -7,7 +7,7 @@ const SectionWrapper = ({ title, options, contentWidth, children }) => {
   return (
     <StyledSectionWrapper data-testid="SectionWrapper" light={options?.[1]} position={options?.[0]}>
       <Title light={options?.[1] && options?.[1] === 'light' ? false : true}>{title}</Title>
-      <Wrapper align={{ justify: 'center', align: 'start' }} contentWidth={contentWidth}>
+      <Wrapper align={{ justify: 'center', align: 'center' }} contentWidth={contentWidth}>
         {children}
       </Wrapper>
     </StyledSectionWrapper>
