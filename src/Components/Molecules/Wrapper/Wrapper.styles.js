ec3592dc3e4cf1e896fc27e43363fbc49c297db8
Darek Mazur <dev@darekmazur.it>
2023-01-07 19:53:42 +0100

    remove unused console log

--- src/Components/Molecules/Wrapper/Wrapper.styles.js
+++ src/Components/Molecules/Wrapper/Wrapper.styles.js
@@ -6,7 +6,6 @@ const justifyOptions = ['start', 'center', 'space-between', 'space-around', 'spa
 const alignOptions = ['stretch', 'center', 'start', 'end'];
 
 export const StyledWrapper = styled.div`
-  ${(props) => console.log({ props })}
   display: ${({ display }) => (display && displayOptions.includes(display) ? display : 'flex')};
   flex-direction: ${({ display, direction }) =>
     direction && directionOptions.includes(direction) ? direction : display === 'flex' || !display ? 'row' : null};
