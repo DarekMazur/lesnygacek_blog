b98c985d6fe37e0bce25904f2ec97c8bc02d8d31
Darek Mazur <dev@darekmazur.it>
2023-01-07 21:23:40 +0100

    add scroll to top after click on footer logo

--- src/Components/Atoms/Logo/Logo.js
+++ src/Components/Atoms/Logo/Logo.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { scrollToTop } from '../../../utils/helpers/scrollToTop';
 import { vectorLogo } from '../../../utils/helpers/vectorLogo';
 import { StyledLogo } from './Logo.styled';
 
 const Logo = ({ logo, light, size }) => {
   return (
-    <StyledLogo light={light} size={size} data-testid={logo}>
+    <StyledLogo light={light} size={size} data-testid={logo} onClick={scrollToTop}>
       {vectorLogo[logo]}
     </StyledLogo>
   );
--- src/Components/Atoms/Logo/Logo.styled.js
+++ src/Components/Atoms/Logo/Logo.styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const StyledLogo = styled.div`
   height: ${({ size }) => (size ? `${size}rem` : `10rem`)};
   width: auto;
+  cursor: pointer;
 
   svg {
     height: 100%;
