812f20a7ae732c4cfa737449efd4b7da3b207749
Darek Mazur <dev@darekmazur.it>
2023-01-08 17:12:25 +0100

    fix center footer logo

--- src/components/Molecules/Copyright/Copyright.styles.js
+++ src/components/Molecules/Copyright/Copyright.styles.js
@@ -5,12 +5,13 @@ export const StyledCopyright = styled.div`
   flex-direction: column;
   justify-content: ${({ pageCopy }) => (pageCopy ? 'center' : 'end')};
   align-items: ${({ pageCopy }) => (pageCopy ? 'center' : 'end')};
-  height: ${({ pageCopy }) => (pageCopy ? null : '100%')};
-  ${'' /* height: 20rem; */}
-  height: 100%;
+  height: ${({ pageCopy }) => (pageCopy ? 'auto' : '100%')};
   color: ${({ theme, pageCopy }) => (pageCopy ? theme.colors.dark : theme.colors.white)};
   font-size: ${({ theme, pageCopy }) => (pageCopy ? theme.fontSize.l : theme.fontSize.m)};
   font-weight: ${({ theme, pageCopy }) => (pageCopy ? theme.fontWeight.regular : theme.fontWeight.bold)};
+  position: ${({ pageCopy }) => (pageCopy ? 'absolute' : 'static')};
+  left: ${({ pageCopy }) => (pageCopy ? '50%' : null)};
+  transform: ${({ pageCopy }) => (pageCopy ? 'translateX(-50%)' : null)};
 
   p {
     padding: 0;
