6a4272447662fc772e6100216d496c4a1169ceb6
Darek Mazur <dev@darekmazur.it>
2023-01-09 13:38:27 +0100

    refactor: project structure

--- src/Components/Atoms/Signature/Signature.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { P } from '../P/P.styles';
-
-const Signature = ({ author }) => {
-  return <P options={{ justify: 'right', width: '100%' }}>{author}</P>;
-};
-
-export default Signature;
