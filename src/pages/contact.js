5598787c1b79ee2b5462ead87abdab468328a83e
Darek Mazur <dev@darekmazur.it>
2023-01-09 14:30:46 +0100

    create contact sketch

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,25 @@
 import * as React from 'react';
 import { ThemeProvider } from 'styled-components';
+import Title from '../components/Atoms/Title/Title';
+import Wrapper from '../components/Molecules/Wrapper/Wrapper';
 import Layout from '../components/Templates/Layout/Layout';
 import { GlobalStyle } from '../styles/globalStyle';
 import { theme } from '../utils/themes/theme';
 
-const ContactPage = () => {
+const ContactPage = ({ sub, light }) => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Layout>
-        <main>
-          <p>lorem ipsum</p>
-        </main>
+        <Wrapper as="main">
+          <Title sub={sub} light={light}>
+            Kontakt
+          </Title>
+        </Wrapper>
+        <Wrapper align={{ justify: 'space-between' }}>
+          <div>kontact message</div>
+          <div>kontact form</div>
+        </Wrapper>
       </Layout>
     </ThemeProvider>
   );
