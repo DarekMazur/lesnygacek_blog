7464247dbe4ac0f25436d899e4176265026ebf11
Darek Mazur <dev@darekmazur.it>
2023-01-30 00:01:03 +0100

    change insta posts count to 8

--- src/components/Organisms/InstaGrid/InstaGrid.styles.js
+++ src/components/Organisms/InstaGrid/InstaGrid.styles.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const StyledGrid = styled.div`
   padding: 3rem 0;
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   grid-gap: 1.5rem;
 `;
--- src/pages/index.js
+++ src/pages/index.js
@@ -68,7 +68,7 @@ const IndexPage = () => {
   useEffect(() => {
     async function fetchData() {
       await fetch(
-        `https://graph.facebook.com/v15.0/${process.env.GATSBY_IG_USER}/media?fields=id,media_type,media_url,shortcode,like_count&limit=6&access_token=${process.env.GATSBY_IG_KEY}`
+        `https://graph.facebook.com/v15.0/${process.env.GATSBY_IG_USER}/media?fields=id,media_type,media_url,shortcode,like_count&limit=8&access_token=${process.env.GATSBY_IG_KEY}`
       )
         .then((res) => {
           if (res.ok) {
