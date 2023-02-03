36b1aef748cf435f398363e875838557e11a8c99
Darek Mazur <dev@darekmazur.it>
2023-02-03 21:05:37 +0100

    create search results page sketch

---
+++ src/pages/search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchResults = () => {
+  <>
+    <h3>Search</h3>
+    <ul>
+      <li>Article</li>
+      <li>Article 2</li>
+    </ul>
+  </>;
+};
+
+export default SearchResults;
